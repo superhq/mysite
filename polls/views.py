4eda2f158111b7f87f51318c99539b4ffd7bebb5
superhq <hongqun@126.com>
2018-05-30 17:31:05 +0800

    polls app

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,26 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Question, Choice
 from django.shortcuts import get_object_or_404, render, reverse
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def index(request):
